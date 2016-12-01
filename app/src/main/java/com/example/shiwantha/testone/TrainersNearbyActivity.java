2042a8107d6da4f6df5420fa93fc1f986b8deee4
shiwantha sandamal <shiwanthafri@gmail.com>
2016-12-02 05:16:35 +0530

    join trainers club button done

--- app/src/main/java/com/example/shiwantha/testone/TrainersNearbyActivity.java
+++ app/src/main/java/com/example/shiwantha/testone/TrainersNearbyActivity.java
@@ -1,5 +1,6 @@
 package com.example.shiwantha.testone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TrainersNearbyActivity extends AppCompatActivity {
 
@@ -32,6 +34,16 @@ public class TrainersNearbyActivity extends AppCompatActivity {
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        //navigate to trainer profile
+        final Button joinTrainersClub = (Button) findViewById(R.id.button2);
+        joinTrainersClub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(TrainersNearbyActivity.this, JoinTrainerClubActivity.class));
+            }
+        });
+
     }
+
 
 }
