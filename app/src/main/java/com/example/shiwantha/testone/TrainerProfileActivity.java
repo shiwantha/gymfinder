a9c3516b67a031232161279af5094f1e9de76a7c
shiwantha sandamal <shiwanthafri@gmail.com>
2016-12-02 03:42:05 +0530

    Added navigation button for more interfaces

--- app/src/main/java/com/example/shiwantha/testone/TrainerProfileActivity.java
+++ app/src/main/java/com/example/shiwantha/testone/TrainerProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.shiwantha.testone;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,6 +26,12 @@ public class TrainerProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 
 }
